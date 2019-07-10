e8bc587d834770d9420b5b05bdbc9070483a1a1b
Alexey <epsilok943@gmail.com>
2019-07-10 06:33:00 +0300

    Display record's details

--- src/views/DetailRecord.vue
+++ src/views/DetailRecord.vue
@@ -1,25 +1,51 @@
 <template>
-    <div>
-  <div>
+<div>
+ <Loader v-if="loading"/>
+  <div v-else-if="record">
     <div class="breadcrumb-wrap">
       <a href="/history" class="breadcrumb">History</a>
-      <a class="breadcrumb">
-        Expense
+      <a @click.prevent class="breadcrumb">
+        {{ record.type === 'income' ? 'Income' : 'Outcome' }}
       </a>
     </div>
     <div class="row">
       <div class="col s12 m6">
-        <div class="card red">
+        <div class="card" :class="{ 
+          'red' : record.type === 'outcome',
+          'green' : record.type === 'income'
+        }">
           <div class="card-content white-text">
-            <p>Description:</p>
-            <p>Total:</p>
-            <p>Category:</p>
+            <p>Description: {{record.description}}</p>
+            <p>Amount: {{record.amount | currency}}</p>
+            <p>Category: {{record.categoryName}}</p>
 
-            <small>SampleExample</small>
+            <small>{{record.date | date('datetime')}}</small>
           </div>
         </div>
       </div>
     </div>
   </div>
+  <span v-else>Record with id: {{$route.params.id}} not found</span>
 </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    record: null,
+    loading: true
+  }),
+  async mounted() {
+    const id = this.$route.params.id
+    const record = await this.$store.dispatch('fetchRecordById', id)
+    const category = await this.$store.dispatch('fetchCategoryById', record.categoryId)
+
+    this.record = {
+      ...record,
+      categoryName: category.title
+    }
+
+    this.loading = false
+  }
+}
+</script>
